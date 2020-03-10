e4e5713686609d6751eafc7ae52e1a1005e2304a
javila35 <josepharthuravila@gmail.com>
2020-03-10 11:14:53 -0700

    First deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+function addTask(newTask) {
+  let task = document.createElement("li");
+  task.innerHTML = `<li>${newTask}</li>`;
+  document.querySelector("ul").appendChild(task);
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log(event.target);
+    const tasktoAdd = event.target["new-task-description"].value;
+    addTask(tasktoAdd);
+  });
+  
 });
