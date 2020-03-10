2c5e631f244c29aa12d2312a88b6fd4bc1790927
javila35 <josepharthuravila@gmail.com>
2020-03-10 15:48:15 -0700

    Adding buttons

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
+const taskList = document.getElementById("tasks")
+
 function addTask(newTask) {
   let task = document.createElement("li");
-  task.innerHTML = `<li>${newTask}</li>`;
-  document.querySelector("ul").appendChild(task);
+  let button = document.createElement("button")
+  button.innerText = "X";
+  button.id= `${newTask}`;
+  task.innerHTML = `${newTask}`;
+  taskList.appendChild(task);
+  task.appendChild(button);
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("create-task-form");
   form.addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log(event.target);
     const tasktoAdd = event.target["new-task-description"].value;
     addTask(tasktoAdd);
   });
-  
 });
